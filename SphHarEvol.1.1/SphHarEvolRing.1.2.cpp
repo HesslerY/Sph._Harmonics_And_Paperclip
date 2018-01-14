8641fad591e5bf4751ac2ef3977d2f7579fa76b6
sGourapura <gourapura.2@osu.edu>
2018-01-14 18:00:34 -0500

    Working on troubleshooting the ring code. I fixed some mistakes in the spherical harmonics function and I think I know what's still wrong, seems to be a mathematical issue involving how you integrate these surfaces. Will need to talk to someone to sort it out.

--- SphHarEvol.1.1/SphHarEvolRing.1.2.cpp
+++ SphHarEvol.1.1/SphHarEvolRing.1.2.cpp
@@ -47,29 +47,29 @@ double integrateSpecies( vector<vector<double> >& pop, int species, double Theta
 //Y00		
 			pop[species][0]*(1/2.0)*(1/sqrt(Pi)) 
 //Y01
-			+pop[species][1]*pow(10,-6)*(1/2.0)*sqrt(3/Pi)*cos(degree)
+			+pop[species][1]*(1/2.0)*sqrt(3/Pi)*cos(degree)
 //Y02
-			+pop[species][2]*(1/4.0)*sqrt(5/Pi)*(3*pow(cos(degree), 2)- 1)
+			+pop[species][2]*(1/4.0)*sqrt(5/Pi)*(3*pow(cos(degree), 2) - 1)
 //Y03
-			+pop[species][3]*pow(10, -7)*(1/4.0)*sqrt(7/Pi)*(5*pow(cos(degree),3)- 3*cos(degree))
+			+pop[species][3]*(1/4.0)*sqrt(7/Pi)*(5*pow(cos(degree),3) - 3*cos(degree))
 //Y04
-			+pop[species][4]*(3/16.0)*sqrt(1/Pi)*(35*pow(cos(degree),4) - 30*pow(cos(degree),2)+3)
+			+pop[species][4]*(3/16.0)*sqrt(1/Pi)*(35*pow(cos(degree),4) - 30*pow(cos(degree),2) + 3)
 //Y05
-			+pop[species][5]*pow(10,-7)*(1/16.0)*sqrt(11/Pi)*(15*cos(degree) - 70*pow(cos(degree),3)+63*pow(cos(degree),5))
+			+pop[species][5]*(1/16.0)*sqrt(11/Pi)*(63*pow(cos(degree),5) - 70*pow(cos(degree),3) + 15*cos(degree))
 //Y06
-			+pop[species][6]*(1/32.0)*sqrt(13/Pi)*(-5 + 105*pow(cos(degree),2)-315*pow(cos(degree),4) + 231*pow(cos(degree),6))
+			+pop[species][6]*(1/32.0)*sqrt(13/Pi)*(231*pow(cos(degree),6) - 315*pow(cos(degree),4) + 105*pow(cos(degree),2) - 5 )
 //Y07					
-			+pop[species][7]*pow(10, -7)*(1/32.0)*sqrt(15/Pi)*(-35*cos(degree)+ 315*pow(cos(degree),3) -693*pow(cos(degree),5) + 429*pow(cos(degree),7))
+			+pop[species][7]*(1/32.0)*sqrt(15/Pi)*(429*pow(cos(degree),7) - 693*pow(cos(degree),5) + 315*pow(cos(degree),3) - 35*cos(degree))
 //Y08					
-			+pop[species][8]*(1/256.0)*sqrt(17/Pi)*(35 - 1260*pow(cos(degree),2) + 6930*pow(cos(degree),4) - 12012*pow(cos(degree),6) + 6435*pow((cos(degree)),8))
+			+pop[species][8]*(1/256.0)*sqrt(17/Pi)*(6435*pow((cos(degree)),8) - 12012*pow(cos(degree),6) + 6930*pow(cos(degree),4) - 1260*pow(cos(degree),2) + 35)
 //Y09
-			+pop[species][9]*(1/256.0)*sqrt(19/Pi)*(315*cos(degree)- 4620*pow(cos(degree),3) + 18018*pow(cos(degree),5) - 25740*pow(cos(degree),7) + 12155*pow((cos(degree)),9))
+			+pop[species][9]*(1/256.0)*sqrt(19/Pi)*(12155*pow((cos(degree)),9) - 25740*pow(cos(degree),7) + 18018*pow(cos(degree),5) - 4620*pow(cos(degree),3) + 315*cos(degree))
 //Y10					
-			+pop[species][10]*(1/512.0)*sqrt(21/Pi)*(-63 +3465*pow(cos(degree),2) - 30030*pow(cos(degree),4) + 90090*pow(cos(degree),6) -109395*pow((cos(degree)),8)+46189*pow(cos(degree),10))		
+			+pop[species][10]*(1/512.0)*sqrt(21/Pi)*(46189*pow(cos(degree),10) - 109395*pow((cos(degree)),8) + 90090*pow(cos(degree),6) - 30030*pow(cos(degree),4) + 3465*pow(cos(degree),2) - 63)		
 //Y011
-			+pop[species][11]*(1/512.0)*sqrt(23/Pi)*(-693*pow(cos(degree),1) +15015*pow(cos(degree),3) - 90090*pow(cos(degree),5) +218790*pow((cos(degree)),7)-230945*pow(cos(degree),9)+88179*pow(cos(degree),11))
+			+pop[species][11]*(1/512.0)*sqrt(23/Pi)*(88179*pow(cos(degree),11) - 230945*pow(cos(degree),9) + 218790*pow((cos(degree)),7) - 90090*pow(cos(degree),5) + 15015*pow(cos(degree),3) - 693*cos(degree))
 //Y012
-			+pop[species][12]*(1/2048.0)*sqrt(25/Pi)*(231 -18018*pow(cos(degree),2) +225225*pow(cos(degree),4) - 1021020*pow(cos(degree),6) +2078505*pow((cos(degree)),8)-1939938*pow(cos(degree),10)+676039*pow(cos(degree),12))
+			+pop[species][12]*(5/2048.0)*sqrt(1/Pi)*(676039*pow(cos(degree),12) - 1939938*pow(cos(degree),10) + 2078505*pow((cos(degree)),8) - 1021020*pow(cos(degree),6) + 225225*pow(cos(degree),4) - 18018*pow(cos(degree),2) + 231)
 			)*sin(degree);
 	}
 // Finally, we give the summed values of the spherical harmonic from theta min to theta max as the test score (a rough kind of integration by .5 degree increments)			
@@ -121,7 +121,7 @@ int main()
 
 // Now, we evolve. We loop the following code for each generation
 	for (int g = 1; g <= Gen; g++){
-		cout << endl << "Generation: " << g << endl;
+	//	cout << endl << "Generation: " << g << endl;
 		
 // The first objective is to calculate how well our current population's species are doing
 		double testScores[100];
