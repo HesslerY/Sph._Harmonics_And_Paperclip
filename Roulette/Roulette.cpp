281b12de5d316be3c385db2561a698cf03133ec6
Ryan Debolt <ryantdebolt@pitzer-rw02.ten.osc.edu>
2020-06-08 15:21:03 -0400

    started writting new roulette

--- Roulette/Roulette.cpp
+++ Roulette/Roulette.cpp
@@ -3,3 +3,16 @@
 // algorithm so see what differences might arise and see if it could be usefull to the current bicone evolution
 
 // Code Starts Here
+// **IMPORTANT** compile using g++ -std=c++11 Roulette.cpp -o Roulette.exe
+
+#include <time.h>
+#include <math.h>
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+using namespace std;
