114033e49793c1d0267f1db20eec112447c498a3
Ryan Debolt <ryantdebolt@pitzer-rw02.ten.osc.edu>
2020-06-08 14:46:30 -0400

    New directory and file for roulette algorithm

---
+++ Roulette/Roulette.cpp
@@ -0,0 +1,5 @@
+// Welcome to the new Paperclips Roulette Algorithm//
+// Our goal is to test this algorithm (based on the bicone's roulette algorithm) against the existing tournament
+// algorithm so see what differences might arise and see if it could be usefull to the current bicone evolution
+
+// Code Starts Here
